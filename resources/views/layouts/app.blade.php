b86ca581043487b69dcff106d35354e4a67a305c
fakhrizafathur <fakhrizafathur05@gmail.com>
2025-12-09 14:58:25 +0700

    Fixing login blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -310,6 +310,22 @@
                 font-size: 1.2rem;
             }
         }
+
+        /* Fix Chrome Autofill */
+        input:-webkit-autofill,
+        input:-webkit-autofill:hover,
+        input:-webkit-autofill:focus,
+        textarea:-webkit-autofill,
+        textarea:-webkit-autofill:hover,
+        textarea:-webkit-autofill:focus,
+        select:-webkit-autofill,
+        select:-webkit-autofill:hover,
+        select:-webkit-autofill:focus {
+            -webkit-box-shadow: 0 0 0px 1000px var(--bs-form-control-bg) inset !important;
+            box-shadow: 0 0 0px 1000px var(--bs-form-control-bg) inset !important;
+            -webkit-text-fill-color: var(--bs-form-control-color) !important;
+            transition: background-color 5000s ease-in-out 0s;
+        }
     </style>
     @stack('head')
 </head>
